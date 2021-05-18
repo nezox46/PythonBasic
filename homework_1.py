ad1af6346c1f6781cc895d748374786c40d2ea47
Evgenii <pix.123@yandex.ru>
2021-05-18 23:03:48 +0300

    Домашка ко 2-ому уроку

--- homework_1.py
+++
@@ -1,4 +0,0 @@
-duration = int(input())
-days, hour, minute, second = duration // 86400 % 3600, duration // 3600 % 24, duration // 60 % 60, duration % 60
-print(f'{days} дней {hour} часов {minute} минут {second} секунд')
-
